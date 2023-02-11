bf32785b798f76f99ffd2e658ec50e4bd54d9a55
ronald <ronald.andrade.02@gmail.com>
2023-02-11 02:27:55 -0300

    JPA folder

---
+++ ORM_and_JPA/src/main/java/com/dio/ORM_and_JPA/OrmAndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.ORM_and_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmAndJpaApplication.class, args);
+	}
+
+}
---
+++ ORM_and_JPA/src/test/java/com/dio/ORM_and_JPA/OrmAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.ORM_and_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
