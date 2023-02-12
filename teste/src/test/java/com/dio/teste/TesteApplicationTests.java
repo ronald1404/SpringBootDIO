a584d3b251feb03bcb68ccf084796d2d9eb6de6e
ronald <ronald.andrade.02@gmail.com>
2023-02-12 16:24:14 -0300

    mysql connection with spring

---
+++ teste/src/test/java/com/dio/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
