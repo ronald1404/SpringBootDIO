dc3221ba2ea7a344d95803b02b54e5d4d25aae50
ronald <ronald.andrade.02@gmail.com>
2023-02-12 16:50:08 -0300

    Using query for search data

--- SpringDataJpa/src/main/java/com/dio/SpringDataJpa/StarApp.java
+++ SpringDataJpa/src/main/java/com/dio/SpringDataJpa/StarApp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StarApp implements CommandLineRunner {
 
@@ -14,21 +16,22 @@ public class StarApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+//
+//        User user = new User();
+//        user.setName("Ronald");
+//        user.setUsername("ronald1404");
+//        user.setPassword("1234");
+//
+//        User userA = new User();
+//        userA.setName("Karol");
+//        userA.setUsername("karol2510");
+//        userA.setPassword("4321");
+//
+//        repository.save(user);
+//        repository.save(userA);
 
-        User user = new User();
-        user.setName("Ronald");
-        user.setUsername("ronald1404");
-        user.setPassword("1234");
-
-        User userA = new User();
-        userA.setName("Karol");
-        userA.setUsername("karol2510");
-        userA.setPassword("4321");
-
-        repository.save(user);
-        repository.save(userA);
-
-
+        List<User> users = repository.filterByName("Ronald");
+        System.out.println(users);
         for(User u: repository.findAll()){
             System.out.println(u);
         }
--- SpringDataJpa/src/main/java/com/dio/SpringDataJpa/repository/UserRepository.java
+++ SpringDataJpa/src/main/java/com/dio/SpringDataJpa/repository/UserRepository.java
@@ -2,8 +2,17 @@ package com.dio.SpringDataJpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.dio.SpringDataJpa.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository <User, Integer>
 {
+    List<User> findByNameContaining(String name);
 
+    User findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE  u.name LIKE %:name%")
+    List<User> filterByName(@Param("name") String name);
 }
