9670f70bc1b60237becec1d673c90563ca14ee00
ronald <ronald.andrade.02@gmail.com>
2023-02-12 16:32:03 -0300

    uncommented

--- SpringDataJpa/src/main/java/com/dio/SpringDataJpa/StarApp.java
+++ SpringDataJpa/src/main/java/com/dio/SpringDataJpa/StarApp.java
@@ -1,36 +1,36 @@
-//package com.dio.SpringDataJpa;
-//
-//import com.dio.SpringDataJpa.model.User;
-//import com.dio.SpringDataJpa.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class StarApp implements CommandLineRunner {
-//
-//    @Autowired
-//    private UserRepository repository;
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//        User user = new User();
-//        user.setName("Ronald");
-//        user.setUsername("ronald1404");
-//        user.setPassword("1234");
-//
-//        User userA = new User();
-//        userA.setName("Karol");
-//        userA.setUsername("karol2510");
-//        userA.setPassword("4321");
-//
-//        repository.save(user);
-//        repository.save(userA);
-//
-//
-//        for(User u: repository.findAll()){
-//            System.out.println(u);
-//        }
-//    }
-//}
+package com.dio.SpringDataJpa;
+
+import com.dio.SpringDataJpa.model.User;
+import com.dio.SpringDataJpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StarApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User user = new User();
+        user.setName("Ronald");
+        user.setUsername("ronald1404");
+        user.setPassword("1234");
+
+        User userA = new User();
+        userA.setName("Karol");
+        userA.setUsername("karol2510");
+        userA.setPassword("4321");
+
+        repository.save(user);
+        repository.save(userA);
+
+
+        for(User u: repository.findAll()){
+            System.out.println(u);
+        }
+    }
+}
--- SpringDataJpa/src/main/java/com/dio/SpringDataJpa/repository/UserRepository.java
+++ SpringDataJpa/src/main/java/com/dio/SpringDataJpa/repository/UserRepository.java
@@ -1,9 +1,9 @@
-//package com.dio.SpringDataJpa.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import com.dio.SpringDataJpa.model.User;
-//
-//public interface UserRepository extends JpaRepository <User, Integer>
-//{
-//
-//}
+package com.dio.SpringDataJpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dio.SpringDataJpa.model.User;
+
+public interface UserRepository extends JpaRepository <User, Integer>
+{
+
+}
