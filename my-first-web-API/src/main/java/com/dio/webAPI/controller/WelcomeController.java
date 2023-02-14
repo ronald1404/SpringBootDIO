34ae51b5c89a345ac46a58551455e731101a6365
ronald <ronald.andrade.02@gmail.com>
2023-02-14 11:14:07 -0300

    update

--- my-first-web-API/src/main/java/com/dio/webAPI/controller/WelcomeController.java
+++ my-first-web-API/src/main/java/com/dio/webAPI/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String welcome(){
         return "Welcome to my first Spring Boot web API";
     }
