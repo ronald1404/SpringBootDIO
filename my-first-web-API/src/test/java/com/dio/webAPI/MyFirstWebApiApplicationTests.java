e1e72bb8e297265d164e360801a242beacf35ce7
ronald <ronald.andrade.02@gmail.com>
2023-02-13 21:28:47 -0300

    Spring REST API

---
+++ my-first-web-API/src/test/java/com/dio/webAPI/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.webAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
