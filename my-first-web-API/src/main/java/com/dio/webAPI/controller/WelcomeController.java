843450244c90db23e8fe12466c7b01e5d1953345
ronald <ronald.andrade.02@gmail.com>
2023-02-13 21:55:45 -0300

    hello in local network

---
+++ my-first-web-API/src/main/java/com/dio/webAPI/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.dio.webAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to my first Spring Boot web API";
+    }
+}
